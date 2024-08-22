83374964a767c8f783d062e1a583074ba220f83b
azouz17 <78298485+azouz17@users.noreply.github.com>
2024-08-22 16:35:42 +0300

    created splash component

--- src/app/Pages/Splash.tsx
+++ src/app/Pages/Splash.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Nasdaq from '../assets/Nasdaq.png'
 import Image from 'next/image'
 import '../globals.css'
+import {Spin} from 'antd'
 
 const Splash = () => {
   return (
-    <div className="splash">
-        <Image className='Nasdaq' src={Nasdaq} alt="nasdaqLogo" width={100} height={100} />
+    <div className="Splash">
+        <Image className='Nasdaq' src={Nasdaq} alt="nasdaqLogo" width={527} height={150} />
+        <p className="subtitle">By Mostafa Azouz</p>
+        <Spin size='large'  className="spin"/>
     </div>
   )
 }
